b3dc016292009955432069072da6bddb5039c258
Weine <weineolliver@gmail.com>
2021-05-08 18:49:55 -0300

    adding prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,43 @@
 module.exports = {
     root: true,
     env: {
-      node: true,
-      es6: true,
+        node: true,
+        es6: true,
     },
     parserOptions: { ecmaVersion: 8 },
     ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js'],
-    extends: ['eslint:recommended'],
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
     overrides: [
-      {
-        files: ['**/*.ts', '**/*.tsx'],
-        parser: '@typescript-eslint/parser',
-        settings: { react: { version: 'detect' } },
-        env: {
-          browser: true,
-          node: true,
-          es6: true,
-        },
-        extends: [
-          'eslint:recommended',
-          'plugin:@typescript-eslint/recommended',
-          'plugin:react/recommended', 
-          'plugin:react-hooks/recommended',
-          'plugin:jsx-a11y/recommended', 
-        ],
-        rules: {
-          'react/prop-types': 'off',  
-          'react/react-in-jsx-scope': 'off',  
-          'jsx-a11y/anchor-is-valid': 'off',  
-          '@typescript-eslint/no-unused-vars': ['error'],
-          '@typescript-eslint/explicit-function-return-type': [
-            'warn',
-            {
-              allowExpressions: true,
-              allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+        {
+            files: ['**/*.ts', '**/*.tsx'],
+            parser: '@typescript-eslint/parser',
+            settings: { react: { version: 'detect' } },
+            env: {
+                browser: true,
+                node: true,
+                es6: true,
             },
-          ],
+            extends: [
+                'eslint:recommended',
+                'plugin:@typescript-eslint/recommended',
+                'plugin:react/recommended',
+                'plugin:react-hooks/recommended',
+                'plugin:jsx-a11y/recommended',
+            ],
+            rules: {
+                'prettier/prettier': ['warn', {}, { usePrettierrc: true }],
+                'react/prop-types': 'off',
+                'react/react-in-jsx-scope': 'off',
+                'jsx-a11y/anchor-is-valid': 'off',
+                '@typescript-eslint/no-unused-vars': ['error'],
+                '@typescript-eslint/explicit-function-return-type': [
+                    'warn',
+                    {
+                        allowExpressions: true,
+                        allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+                    },
+                ],
+            },
         },
-      },
     ],
-  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import { NextPage } from 'next';
+import { NextPage } from 'next'
 
 const IndexPage: NextPage = () => {
     return (
-      <main>
-        <h1> Hello World </h1>
-      </main>
+        <main>
+            <h1> Hello World </h1>
+        </main>
     )
-  }
+}
 
 export default IndexPage
